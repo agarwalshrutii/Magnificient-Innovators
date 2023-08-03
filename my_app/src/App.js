b5e040e037e2c22895fa4b3a72c304843e218e6a
agarwalshrutii <137428487+agarwalshrutii@users.noreply.github.com>
2023-08-03 20:59:11 +0530

    file updation

---
+++ my_app/src/App.js
@@ -0,0 +1,17 @@
+
+//import Loginform from './components/components/login/login';
+import Header from './components/header/header';
+import Navbar from './components/navbar/navbar';
+import Homepage from './components/pages/home/home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Homepage/>
+
+    </div>
+  );
+}
+
+export default App;
