2df6de4213f3be222b99be9ffa3c37b05faf902a
agarwalshrutii <137428487+agarwalshrutii@users.noreply.github.com>
2023-08-03 21:00:45 +0530

    files updation

---
+++ my_app/src/components/footer/footer.jsx
@@ -0,0 +1,16 @@
+import "./footer.css";
+
+
+export default function footer()
+{
+  return (
+
+   <footer>
+        <div class="footer-bottom">
+            <p>Copyright &copy;2023. Designed by <span>Magnificent Innovators </span></p>
+        </div>
+        </footer>
+
+
+  )
+}
